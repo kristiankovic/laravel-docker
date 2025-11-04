5bea9bf1740d0b6736d255a40f64150fe3f13555
kristiankovic <v23011@ues.edu.sv>
2025-11-03 22:59:34 -0600

    crear nuevo proyecto

---
+++ clases-tpi-laravel/proyectos/parcial1/resources/views/components/navbar.blade.php
@@ -0,0 +1,5 @@
+<!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
+
+
+
